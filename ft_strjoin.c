3517e8ce111e6d39728e202c95465b263a8eab97
Rik Verhoeven <rikverhoeven10@gmail.com>
2024-08-01 12:21:27 +0200

    changed strjoin, returns s2 if s1==NULL. To suit minishell

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -12,31 +12,43 @@
 
 #include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+static void	build_str(char **strjoined, char const *s1, char const *s2)
 {
-	char			*strjoined;
 	unsigned int	i;
 	unsigned int	j;
 
-	if (!s1 || !s2)
-		return (NULL);
-	strjoined = (char *)malloc(sizeof(char)
-			* (ft_strlen((char *)s1) + ft_strlen((char *)s2) + 1));
-	if (strjoined == NULL)
-		return (NULL);
 	i = 0;
 	while (s1[i])
 	{
-		strjoined[i] = s1[i];
+		(*strjoined)[i] = s1[i];
 		i++;
 	}
 	j = 0;
 	while (s2[j])
 	{
-		strjoined[j + i] = s2[j];
+		(*strjoined)[j + i] = s2[j];
 		j++;
 	}
-	strjoined[j + i] = '\0';
+	(*strjoined)[j + i] = '\0';
+}
+
+/**
+ * if s1 is NULL, builds new copy of s2 and returns it
+ * otherwise if both exists, s2 concatenates on s1
+*/
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char			*strjoined;
+
+	if (!s1 && !s2)
+		return (NULL);
+	else if (!s1)
+		return (ft_strdup(s2));
+	strjoined = (char *)malloc(sizeof(char)
+			* (ft_strlen((char *)s1) + ft_strlen((char *)s2) + 1));
+	if (strjoined == NULL)
+		return (NULL);
+	build_str(&strjoined, s1, s2);
 	return (strjoined);
 }
 //int main()
@@ -44,7 +56,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 //	char str1[] = "test";
 //	char str2[] = "123";
 //	char *str3;
-//	str3 = ft_strjoin(str1, str2);
+//	str3 = ft_strjoin_v2(str1, str2);
 //	printf("%s", str3);
 //	free(str3);
 //}
